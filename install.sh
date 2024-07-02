494f81e3eeeec298171bc1a9809fe9b806f0eef5
shenlvyu <shenlvyu@mail.teletraan.io>
2024-07-02 21:38:18 +0800

    feat(install):install

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# 下载并解压缩可执行文件
+wget https://github.com/sly17zql/low/releases/download/v0.0.1/cli88.tar.gz
+
+if [ $? -ne 0 ]; then
+  echo "Download failed. Exiting."
+  exit 1
+fi
+
+tar zxvf cli88.tar.gz cli88
+
+# 移动可执行文件到系统的bin目录
+sudo mv cli88 /usr/local/bin/
+
+# 清理
+rm cli88.tar.gz
+
+echo "cli88 已成功安装"
